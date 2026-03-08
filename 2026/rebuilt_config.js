c391336e03e3bbb298b15a2bed8852763100967e
warbotics21848 <warbotics21848@gmail.com>
2026-03-07 18:37:53 -0600

    Update rebuilt_config.js to GNR event

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026ilch",
+      "defaultValue": "2026ndgf",
       "required": "true"
     },
     { "name": "Match Level",
