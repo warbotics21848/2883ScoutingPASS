97f71c13642afcfd11daf3e31d391ccd4b7ef8b1
heepdog <16630599+heepdog@users.noreply.github.com>
2026-04-29 21:23:55 -0500

    Change defaultValue from '2026DALY' to '2026DAL'

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026DALY",
+      "defaultValue": "2026DAL",
       "required": "true"
     },
     { "name": "Match Level",
